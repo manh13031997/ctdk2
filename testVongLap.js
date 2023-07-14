eb56f3c239b2ca6e360e81e3c180aef9b7395c8c
manh13031997 <nguyenducmanh130397@gmail.com>
2023-07-14 11:06:00 +0700

    for

---
+++ testVongLap.js
@@ -0,0 +1,24 @@
+function lap() {
+   let a=""
+   // let b=['M','A','N','H']
+    //a+=b[0];
+    //a+=b[1];
+    //a+=b[2];
+    //a+=b[3];
+    //document.write(a)
+
+
+   // for (i=0; i<b.length;i++){
+     //   a+=b[i];
+    //}
+    //document.write(a);
+
+
+
+
+
+    for (i=0;i<5;i++){
+        a+="con số"+i+"<br>";
+    }
+    document.write(a)
+}
